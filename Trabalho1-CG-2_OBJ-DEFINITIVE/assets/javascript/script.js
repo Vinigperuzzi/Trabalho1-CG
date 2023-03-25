a0272120eb6c75a0a79a59f8d46ab3ebac8fd877
Vinigperuzzi <vinigperuzzi@hotmail.com>
2023-03-25 04:35:13 -0300

    Acho que não vai dar :(

---
+++ Trabalho1-CG-2_OBJ-DEFINITIVE/assets/javascript/script.js
@@ -0,0 +1,396 @@
+"use strict";
+
+
+let nomeBemVido = window.prompt("Informe o seu nome para uma experiência mais personalizada!!");
+const nomeP = document.querySelector("#h1-msg");
+if (nomeBemVido == null || nomeBemVido == ""){
+    nomeBemVido = 'Visitante';
+}
+nomeP.innerHTML += `, ${nomeBemVido}!`;
+
+var vertexShaderSource = `#version 300 es
+
+// an attribute is an input (in) to a vertex shader.
+// It will receive data from a buffer
+in vec4 a_position;
+in vec4 a_color;
+
+// A matrix to transform the positions by
+uniform mat4 u_matrix;
+
+// a varying the color to the fragment shader
+out vec4 v_color;
+
+// all shaders have a main function
+void main() {
+  // Multiply the position by the matrix.
+  gl_Position = u_matrix * a_position;
+
+  // Pass the color to the fragment shader.
+  v_color = a_color;
+}
+`;
+
+var fragmentShaderSource = `#version 300 es
+
+precision highp float;
+
+// the varied color passed from the vertex shader
+in vec4 v_color;
+
+// we need to declare an output for the fragment shader
+out vec4 outColor;
+
+void main() {
+  outColor = v_color;
+}
+`;
+
+
+function createShader(gl, type, source){
+  let shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+      return shader;
+  }
+  console.log(gl.getShaderInfoLog(sha));
+  gl.deleteShader(shader);
+}
+  
+function createProgram(gl, vertexShader, fragmentShader){
+  let program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  let success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+      return program;
+  }
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+function criaItem(index, color){
+  return {
+    index,
+    color
+  };
+}
+let carrinhoQTD = 0;
+
+let itensCarrinho = [];
+
+function pegaContexto(index, color) {
+  carrinhoQTD++;
+  itensCarrinho.push(criaItem(index, color));
+  console.log(itensCarrinho);
+  console.log(carrinhoQTD);
+  carrinho();
+}
+
+function carrinho (){
+  let tx = 110; let ty = 60; let tz = 0;
+  let rx = 1; let ry = 0; let rz = 0;
+  let sx = 1; let sy = 1; let sz = 1;
+  let index = 0;
+  draw(tx, ty, tz, rx, ry, rz, sx, sy, sz, index);
+}
+
+function main() {
+
+  for (let i = 0; i<10; i++){
+    let name = `#canvas${i}`;
+    let tx = 110; let ty = 60; let tz = 0;
+    let rx = 5; let ry = 0; let rz = -5;
+    let sx = 0.6; let sy = 0.6; let sz = 0.6;
+    let index = i%5;
+    let animation = true;
+    draw1(i, name, tx, ty, tz, rx, ry, rz, sx, sy, sz, index, animation);
+  }
+  for (let i = 0; i<5; i++){
+    let nameItem = `#item-canvas${i}`;
+    let tx = 550; let ty = 220; let tz = 0;
+    let rx = 1; let ry = 0; let rz = 0;
+    let sx = 1; let sy = 1; let sz = 1;
+    let index = i%5;
+    let animation = false;
+    draw1(i, nameItem, tx, ty, tz, rx, ry, rz, sx, sy, sz, index, animation);
+  }
+  carrinho();
+
+}
+
+/*A chamada da função é feita passando:
+O nome do canvas a ser desenhado;
+O deslocamento em X, Y e Z;
+A rotação em X, Y e Z;
+A escala em X, Y e Z;
+E um boleano se deve animar ou não (Talvez evolua para um inteiro para setar mais de um tipo de animação)
+ */
+function draw1(i, canvasName, tx, ty, tz, rx, ry, rz, sx, sy, sz, index, animation){
+  let canvas = document.querySelector(`${canvasName}`);
+  let gl = canvas.getContext("webgl2");
+  if (!gl) {
+    return;
+  }
+  let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+  let program = createProgram(gl, vertexShader, fragmentShader);
+  let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  let colorAttributeLocation = gl.getAttribLocation(program, "a_color");
+  let matrixLocation = gl.getUniformLocation(program, "u_matrix");
+  let positionBuffer = gl.createBuffer();
+  let vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  /*Talvez o bagulho seja a aprtir daqui*/
+
+  let count = setGeometry(gl, index);      //ARRUMAR UM JEITO DE PASSAR ISSO POR PARÂMETRO, OU SELECIONAR POR PARÂMETRO
+
+  var size = 3;          
+  var type = gl.FLOAT;   
+  var normalize = false; 
+  var stride = 0;        
+  var offset = 0;        
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+  
+  let colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  setColors(gl, index, 0);
+
+  gl.enableVertexAttribArray(colorAttributeLocation);
+
+  var size = 3;          
+  var type = gl.UNSIGNED_BYTE;   
+  var normalize = true;  
+  var stride = 0;        
+  var offset = 0;        
+  gl.vertexAttribPointer(
+      colorAttributeLocation, size, type, normalize, stride, offset);
+
+  /*Se pá até aqui*/
+
+  function radToDeg(r) {
+    return r * 180 / Math.PI;
+  }
+  
+  function degToRad(d) {
+    return d * Math.PI / 180;
+  }
+  
+  let translation = [tx, ty, tz];
+  let rotation = [degToRad(rx), degToRad(ry), degToRad(rz)];
+  let scale = [sx, sy, sz];
+
+  drawScene();
+
+  webglLessonsUI.setupSlider(`#x${i}`,      {value: translation[0], slide: updatePosition(0), max: gl.canvas.width });
+  webglLessonsUI.setupSlider(`#y${i}`,      {value: translation[1], slide: updatePosition(1), max: gl.canvas.height});
+  webglLessonsUI.setupSlider(`#z${i}`,      {value: translation[2], slide: updatePosition(2), max: gl.canvas.height});
+  webglLessonsUI.setupSlider(`#angleX${i}`, {value: radToDeg(rotation[0]), slide: updateRotation(0), max: 360});
+  webglLessonsUI.setupSlider(`#angleY${i}`, {value: radToDeg(rotation[1]), slide: updateRotation(1), max: 360});
+  webglLessonsUI.setupSlider(`#angleZ${i}`, {value: radToDeg(rotation[2]), slide: updateRotation(2), max: 360});
+  webglLessonsUI.setupSlider(`#scaleX${i}`, {value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2});
+  webglLessonsUI.setupSlider(`#scaleY${i}`, {value: scale[1], slide: updateScale(1), min: -5, max: 5, step: 0.01, precision: 2});
+  webglLessonsUI.setupSlider(`#scaleZ${i}`, {value: scale[2], slide: updateScale(2), min: -5, max: 5, step: 0.01, precision: 2});
+
+  function updatePosition(index) {
+    return function(event, ui) {
+      translation[index] = ui.value;
+      drawScene();
+    };
+  }
+
+  function updateRotation(index) {
+    return function(event, ui) {
+      var angleInDegrees = ui.value;
+      var angleInRadians = degToRad(angleInDegrees);
+      rotation[index] = angleInRadians;
+      drawScene();
+    };
+  }
+
+  function updateScale(index) {
+    return function(event, ui) {
+      scale[index] = ui.value;
+      drawScene();
+    };
+  }
+  
+  var then = 0;
+
+  function drawScene() {
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+  
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  
+    gl.clearColor(1, 1, 1, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  
+    gl.enable(gl.DEPTH_TEST);
+  
+    gl.enable(gl.CULL_FACE);
+  
+    gl.useProgram(program);
+  
+    gl.bindVertexArray(vao);
+  
+    var matrix = m4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
+    matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
+    matrix = m4.xRotate(matrix, rotation[0]);
+    matrix = m4.yRotate(matrix, rotation[1]);
+    matrix = m4.zRotate(matrix, rotation[2]);
+    matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+  
+    gl.uniformMatrix4fv(matrixLocation, false, matrix);
+  
+    let primitiveType = gl.TRIANGLES;
+    let offset = 0;
+    //let count agora é o retorno da função setGeometry();
+    gl.drawArrays(primitiveType, offset, count);
+    
+    if (animation){ // && mutex (setando ao final da execução e sendo desativado no boilerplate)
+      rotation[1] += degToRad(1);
+      matrix = m4.yRotate(matrix, rotation[1])
+      requestAnimationFrame(drawScene);
+    }
+
+  }  
+}
+
+
+//================================================================================================
+//Início do carrinho
+//================================================================================================
+
+function draw(tx, ty, tz, rx, ry, rz, sx, sy, sz, index){
+  let canvas = document.querySelector(`#canvas-carrinho`);
+  let gl = canvas.getContext("webgl2");
+  if (!gl) {
+    return;
+  }
+  let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+  let program = createProgram(gl, vertexShader, fragmentShader);
+  let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  let colorAttributeLocation = gl.getAttribLocation(program, "a_color");
+  let matrixLocation = gl.getUniformLocation(program, "u_matrix");
+  let positionBuffer = gl.createBuffer();
+  let vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  count = setGeometry(gl, index);      //ARRUMAR UM JEITO DE PASSAR ISSO POR PARÂMETRO, OU SELECIONAR POR PARÂMETRO
+
+  var size = 3;          
+  var type = gl.FLOAT;   
+  var normalize = false; 
+  var stride = 0;        
+  var offset = 0;        
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+  
+  let colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  setColors(gl, index, 0);
+
+  gl.enableVertexAttribArray(colorAttributeLocation);
+
+  var size = 3;          
+  var type = gl.UNSIGNED_BYTE;   
+  var normalize = true;  
+  var stride = 0;        
+  var offset = 0;        
+  gl.vertexAttribPointer(
+      colorAttributeLocation, size, type, normalize, stride, offset);
+
+  function radToDeg(r) {
+    return r * 180 / Math.PI;
+  }
+  
+  function degToRad(d) {
+    return d * Math.PI / 180;
+  }
+  
+  let translation = [tx, ty, tz];
+  let rotation = [degToRad(rx), degToRad(ry), degToRad(rz)];
+  let scale = [sx, sy, sz];
+
+  
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  
+  gl.clearColor(1, 1, 1, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.enable(gl.DEPTH_TEST);
+  
+  gl.enable(gl.CULL_FACE);
+  
+  for (let i = 0; i < carrinhoQTD; i++){
+    count = setGeometry(gl, itensCarrinho[i].index); 
+    var size = 3;          
+    var type = gl.FLOAT;   
+    var normalize = false; 
+    var stride = 0;        
+    var offset = 0;        
+    gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+    
+    let colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    setColors(gl, index, 0);
+  
+    gl.enableVertexAttribArray(colorAttributeLocation);
+  
+    var size = 3;          
+    var type = gl.UNSIGNED_BYTE;   
+    var normalize = true;  
+    var stride = 0;        
+    var offset = 0;        
+    gl.vertexAttribPointer(
+        colorAttributeLocation, size, type, normalize, stride, offset);
+
+    drawScene();
+    if (i%2 == 0){
+      scale[0] = -1;
+      translation[0] += 650;
+    } else {
+      scale[0] = 1;
+      translation[0] -= 650;
+      translation[1] += 160;
+    }
+  }   
+
+
+  function drawScene() {
+  
+  
+  
+    gl.useProgram(program);
+  
+    gl.bindVertexArray(vao);
+  
+    var matrix = m4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
+    matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
+    matrix = m4.xRotate(matrix, rotation[0]);
+    matrix = m4.yRotate(matrix, rotation[1]);
+    matrix = m4.zRotate(matrix, rotation[2]);
+    matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+  
+    gl.uniformMatrix4fv(matrixLocation, false, matrix);
+  
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    gl.drawArrays(primitiveType, offset, count);
+
+  }  
+}
+
+//================================================================================================
+//Fim do carrinho
+//================================================================================================
+
+main();
+
---
+++ Trabalho1-CG-2_OBJ-DEFINITIVE/assets/javascript/script_OBJ.js
@@ -0,0 +1,435 @@
+"use strict";
+
+let nomeBemVido = window.prompt("Informe o seu nome para uma experiência mais personalizada!!");
+const nomeP = document.querySelector("#h1-msg");
+if (nomeBemVido == null || nomeBemVido == ""){
+    nomeBemVido = 'Visitante';
+}
+nomeP.innerHTML += `, ${nomeBemVido}!`;
+
+
+
+
+function parseOBJ(text) {
+  // because indices are base 1 let's just fill in the 0th data
+  const objPositions = [[0, 0, 0]];
+  const objTexcoords = [[0, 0]];
+  const objNormals = [[0, 0, 0]];
+
+  // same order as `f` indices
+  const objVertexData = [
+    objPositions,
+    objTexcoords,
+    objNormals,
+  ];
+
+  // same order as `f` indices
+  let webglVertexData = [
+    [],   // positions
+    [],   // texcoords
+    [],   // normals
+  ];
+
+  const materialLibs = [];
+  const geometries = [];
+  let geometry;
+  let groups = ['default'];
+  let material = 'default';
+  let object = 'default';
+
+  const noop = () => {};
+
+  function newGeometry() {
+    // If there is an existing geometry and it's
+    // not empty then start a new one.
+    if (geometry && geometry.data.position.length) {
+      geometry = undefined;
+    }
+  }
+
+  function setGeometry() {
+    if (!geometry) {
+      const position = [];
+      const texcoord = [];
+      const normal = [];
+      webglVertexData = [
+        position,
+        texcoord,
+        normal,
+      ];
+      geometry = {
+        object,
+        groups,
+        material,
+        data: {
+          position,
+          texcoord,
+          normal,
+        },
+      };
+      geometries.push(geometry);
+    }
+  }
+
+  function addVertex(vert) {
+    const ptn = vert.split('/');
+    ptn.forEach((objIndexStr, i) => {
+      if (!objIndexStr) {
+        return;
+      }
+      const objIndex = parseInt(objIndexStr);
+      const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
+      webglVertexData[i].push(...objVertexData[i][index]);
+    });
+  }
+
+  const keywords = {
+    v(parts) {
+      objPositions.push(parts.map(parseFloat));
+    },
+    vn(parts) {
+      objNormals.push(parts.map(parseFloat));
+    },
+    vt(parts) {
+      // should check for missing v and extra w?
+      objTexcoords.push(parts.map(parseFloat));
+    },
+    f(parts) {
+      setGeometry();
+      const numTriangles = parts.length - 2;
+      for (let tri = 0; tri < numTriangles; ++tri) {
+        addVertex(parts[0]);
+        addVertex(parts[tri + 1]);
+        addVertex(parts[tri + 2]);
+      }
+    },
+    s: noop,    // smoothing group
+    mtllib(parts, unparsedArgs) {
+      // the spec says there can be multiple filenames here
+      // but many exist with spaces in a single filename
+      materialLibs.push(unparsedArgs);
+    },
+    usemtl(parts, unparsedArgs) {
+      material = unparsedArgs;
+      newGeometry();
+    },
+    g(parts) {
+      groups = parts;
+      newGeometry();
+    },
+    o(parts, unparsedArgs) {
+      object = unparsedArgs;
+      newGeometry();
+    },
+  };
+
+  const keywordRE = /(\w*)(?: )*(.*)/;
+  const lines = text.split('\n');
+  for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+    const line = lines[lineNo].trim();
+    if (line === '' || line.startsWith('#')) {
+      continue;
+    }
+    const m = keywordRE.exec(line);
+    if (!m) {
+      continue;
+    }
+    const [, keyword, unparsedArgs] = m;
+    const parts = line.split(/\s+/).slice(1);
+    const handler = keywords[keyword];
+    if (!handler) {
+      console.warn('unhandled keyword:', keyword);  // eslint-disable-line no-console
+      continue;
+    }
+    handler(parts, unparsedArgs);
+  }
+
+  // remove any arrays that have no entries.
+  for (const geometry of geometries) {
+    geometry.data = Object.fromEntries(
+        Object.entries(geometry.data).filter(([, array]) => array.length > 0));
+  }
+
+  return {
+    geometries,
+    materialLibs,
+  };
+}
+
+
+function main(){
+  for (let i = 0; i<10; i++){
+    let name = `#canvas${i}`;
+    let objPath;
+    let animate = true;
+    switch (i%5){
+      case 0: objPath = "assets/obj/airplane1/ap.obj";
+      break;
+      case 1: objPath = "assets/obj/airplane2/jp.obj";
+      break;
+      case 2: objPath = "assets/obj/costerGuard/hc.obj";
+      break;
+      case 3: objPath = "assets/obj/fighter/fgt.obj";
+      break;
+      case 4: objPath = "assets/obj/Heli3/Mi28.obj";
+      break;
+      default: objPath = "assets/obj/Heli3/Mi28.obj";
+      break;
+    }
+    draw(name, objPath, i, animate);
+  }
+  for (let i = 0; i<5; i++){
+    let nameItem = `#item-canvas${i}`;
+    let objPath;
+    let animate = false;
+    switch (i%5){
+      case 0: objPath = "assets/obj/airplane1/ap.obj";
+      break;
+      case 1: objPath = "assets/obj/airplane2/jp.obj";
+      break;
+      case 2: objPath = "assets/obj/costerGuard/hc.obj";
+      break;
+      case 3: objPath = "assets/obj/fighter/fgt.obj";
+      break;
+      case 4: objPath = "assets/obj/Heli3/Mi28.obj";
+      break;
+      default: objPath = "assets/obj/Heli3/Mi28.obj";
+      break;
+    }
+    draw(nameItem, objPath, i, animate);
+  }
+}
+
+/*Isso eu consigo modularizar recebendo o nome do arquivo obj e o nome do canvas
+Só devo passar para o response abaixo do meshprogram
+Talvez seja possível colocar o parser em um arquivo separado*/
+async function draw(name, objPath, i, animate) {
+  // Get A WebGL context
+  /** @type {HTMLCanvasElement} */
+  const canvas = document.querySelector(name);
+  const gl = canvas.getContext("webgl2");
+  if (!gl) {
+    return;
+  }
+
+  // Tell the twgl to match position with a_position etc..
+  twgl.setAttributePrefix("a_");
+
+  const vs = `#version 300 es
+  in vec4 a_position;
+  in vec3 a_normal;
+
+  uniform mat4 u_projection;
+  uniform mat4 u_view;
+  uniform mat4 u_world;
+
+  out vec3 v_normal;
+
+  void main() {
+    gl_Position = u_projection * u_view * u_world * a_position;
+    v_normal = mat3(u_world) * a_normal;
+  }
+  `;
+
+  const fs = `#version 300 es
+  precision highp float;
+
+  in vec3 v_normal;
+
+  uniform vec4 u_diffuse;
+  uniform vec3 u_lightDirection;
+
+  out vec4 outColor;
+
+  void main () {
+    vec3 normal = normalize(v_normal);
+    float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
+    outColor = vec4(u_diffuse.rgb * fakeLight, u_diffuse.a);
+  }
+  `;
+
+
+  // compiles and links the shaders, looks up attribute and uniform locations
+  const meshProgramInfo = twgl.createProgramInfo(gl, [vs, fs]);
+
+  const response = await fetch(objPath);  
+  const text = await response.text();
+  const obj = parseOBJ(text);
+
+  const parts = obj.geometries.map(({data}) => {
+    // Because data is just named arrays like this
+    //
+    // {
+    //   position: [...],
+    //   texcoord: [...],
+    //   normal: [...],
+    // }
+    //
+    // and because those names match the attributes in our vertex
+    // shader we can pass it directly into `createBufferInfoFromArrays`
+    // from the article "less code more fun".
+
+    // create a buffer for each array by calling
+    // gl.createBuffer, gl.bindBuffer, gl.bufferData
+    const bufferInfo = twgl.createBufferInfoFromArrays(gl, data);
+    const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
+    return {
+      material: {
+        u_diffuse: [Math.random(), Math.random(), Math.random(), 1],
+      },
+      bufferInfo,
+      vao,
+    };
+  });
+
+  function getExtents(positions) {
+    const min = positions.slice(0, 3);
+    const max = positions.slice(0, 3);
+    for (let i = 3; i < positions.length; i += 3) {
+      for (let j = 0; j < 3; ++j) {
+        const v = positions[i + j];
+        min[j] = Math.min(v, min[j]);
+        max[j] = Math.max(v, max[j]);
+      }
+    }
+    return {min, max};
+  }
+
+  function getGeometriesExtents(geometries) {
+    return geometries.reduce(({min, max}, {data}) => {
+      const minMax = getExtents(data.position);
+      return {
+        min: min.map((min, ndx) => Math.min(minMax.min[ndx], min)),
+        max: max.map((max, ndx) => Math.max(minMax.max[ndx], max)),
+      };
+    }, {
+      min: Array(3).fill(Number.POSITIVE_INFINITY),
+      max: Array(3).fill(Number.NEGATIVE_INFINITY),
+    });
+  }
+
+  const extents = getGeometriesExtents(obj.geometries);
+  const range = m4.subtractVectors(extents.max, extents.min);
+  // amount to move the object so its center is at the origin
+  const objOffset = m4.scaleVector(
+      m4.addVectors(
+        extents.min,
+        m4.scaleVector(range, 0.5)),
+      -1);
+  const cameraTarget = [0, 0, 0];
+  // figure out how far away to move the camera so we can likely
+  // see the object.
+  const radius = m4.length(range) * 1.2;
+  const cameraPosition = m4.addVectors(cameraTarget, [
+    0,
+    0,
+    radius,
+  ]);
+  // Set zNear and zFar to something hopefully appropriate
+  // for the size of this object.
+  const zNear = radius / 100;
+  const zFar = radius * 3;
+
+  function radToDeg(r) {
+    return r * 180 / Math.PI;
+  }
+
+  function degToRad(deg) {
+    return deg * Math.PI / 180;
+  }
+
+  let translation = [0, 0, 0];
+  let rotation = [degToRad(0), degToRad(0), degToRad(0)];
+  let scale = [1, 1, 1];
+
+  webglLessonsUI.setupSlider(`#x${i}`,      {value: translation[0], slide: updatePosition(0), max: gl.canvas.width });
+  webglLessonsUI.setupSlider(`#y${i}`,      {value: translation[1], slide: updatePosition(1), max: gl.canvas.height});
+  webglLessonsUI.setupSlider(`#z${i}`,      {value: translation[2], slide: updatePosition(2), max: gl.canvas.height});
+  webglLessonsUI.setupSlider(`#angleX${i}`, {value: radToDeg(rotation[0]), slide: updateRotation(0), max: 360});
+  webglLessonsUI.setupSlider(`#angleY${i}`, {value: radToDeg(rotation[1]), slide: updateRotation(1), max: 360});
+  webglLessonsUI.setupSlider(`#angleZ${i}`, {value: radToDeg(rotation[2]), slide: updateRotation(2), max: 360});
+  webglLessonsUI.setupSlider(`#scaleX${i}`, {value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2});
+  webglLessonsUI.setupSlider(`#scaleY${i}`, {value: scale[1], slide: updateScale(1), min: -5, max: 5, step: 0.01, precision: 2});
+  webglLessonsUI.setupSlider(`#scaleZ${i}`, {value: scale[2], slide: updateScale(2), min: -5, max: 5, step: 0.01, precision: 2});
+
+  function updatePosition(index) {
+    return function(event, ui) {
+      translation[index] = ui.value;
+      render();
+    };
+  }
+
+  function updateRotation(index) {
+    return function(event, ui) {
+      var angleInDegrees = ui.value;
+      var angleInRadians = degToRad(angleInDegrees);
+      rotation[index] = angleInRadians;
+      render();
+    };
+  }
+
+  function updateScale(index) {
+    return function(event, ui) {
+      scale[index] = ui.value;
+      render();
+    };
+  }
+
+  function render(time) {
+    time *= 0.001;  // convert to seconds
+
+    twgl.resizeCanvasToDisplaySize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(1, 1, 1, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
+    const fieldOfViewRadians = degToRad(60);
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const projection = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
+
+    const up = [0, 1, 0];
+    // Compute the camera's matrix using look at.
+    const camera = m4.lookAt(cameraPosition, cameraTarget, up);
+
+    // Make a view matrix from the camera matrix.
+    const view = m4.inverse(camera);
+
+    const sharedUniforms = {
+      u_lightDirection: m4.normalize([-1, 3, 5]),
+      u_view: view,
+      u_projection: projection,
+    };
+
+    gl.useProgram(meshProgramInfo.program);
+
+    // calls gl.uniform
+    twgl.setUniforms(meshProgramInfo, sharedUniforms);
+
+    // compute the world matrix once since all parts
+    // are at the same space.
+    let u_world = m4.yRotation(time);
+    u_world = m4.translate(u_world, ...objOffset);
+
+    
+
+    for (const {bufferInfo, vao, material} of parts) {
+      // set the attributes for this part.
+      gl.bindVertexArray(vao);
+      // calls gl.uniform
+      twgl.setUniforms(meshProgramInfo, {
+        u_world,
+        u_diffuse: material.u_diffuse,
+      });
+      // calls gl.drawArrays or gl.drawElements
+      twgl.drawBufferInfo(gl, bufferInfo);
+    }
+
+    if (animate){
+      requestAnimationFrame(render);
+    }
+  }
+  requestAnimationFrame(render);
+}
+
+main();
