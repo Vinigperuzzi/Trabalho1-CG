1532f3559c4355ed06ec5a2dcaf0b01554bd8e95
Vinigperuzzi <vinigperuzzi@hotmail.com>
2023-03-27 23:02:06 -0300

    Que carrinho horroroso!!! :0

--- Trabalho1-CG-2_OBJ-DEFINITIVE/assets/javascript/candidato.js
+++ Trabalho1-CG-2_OBJ-DEFINITIVE/assets/javascript/candidato.js
@@ -23,6 +23,11 @@ function pegaContexto(obj, index) {
   itensCarrinho.push(criaItem(obj, texturaGlobal[index]));
   console.log(itensCarrinho);
   console.log(carrinhoQTD);
+  carrinho();
+}
+
+function carrinho(){
+  drawCar();
 }
 
 let texturaGlobal = ["assets/obj/airplane1/apA.png",
@@ -363,6 +368,221 @@ async function draw(name, objPath, pngPath, orientation, initialScale, i, animat
     mutex = false;
   }
   requestAnimationFrame(render);
+}
+
+
+async function drawCar() {
+  // Get A WebGL context
+  /** @type {HTMLCanvasElement} */
+  const canvas = document.querySelector(`#canvas-carrinho`);
+  const gl = canvas.getContext("webgl2");
+  if (!gl) {
+    return;
+  }
+
+  // Tell the twgl to match position with a_position etc..
+  twgl.setAttributePrefix("a_");
+
+  // compiles and links the shaders, looks up attribute and uniform locations
+  const meshProgramInfo = twgl.createProgramInfo(gl, [vs, fs]);
+
+  /*Provavelmente a partir daqui
+  Pelo menos o parts deve ser vetor*/
+
+  let globalRange;
+  let globalParts = [];
+  for (let i = 0; i<carrinhoQTD; i++){
+    const response = await fetch(itensCarrinho[i].obj);
+    const text = await response.text();
+    const obj = parseOBJ(text);
+
+    const parts = obj.geometries.map(({data}) => {
+
+      // create a buffer for each array by calling
+      // gl.createBuffer, gl.bindBuffer, gl.bufferData
+      const bufferInfo = twgl.createBufferInfoFromArrays(gl, data);
+      const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
+      return {
+        material: {
+          u_diffuse: [Math.random(), Math.random(), Math.random(), 1],
+        },
+        bufferInfo,
+        vao,
+      };
+    });
+    
+    globalParts[i] = parts;
+
+    function getExtents(positions) {
+      const min = positions.slice(0, 3);
+      const max = positions.slice(0, 3);
+      for (let i = 3; i < positions.length; i += 3) {
+        for (let j = 0; j < 3; ++j) {
+          const v = positions[i + j];
+          min[j] = Math.min(v, min[j]);
+          max[j] = Math.max(v, max[j]);
+        }
+      }
+      return {min, max};
+    }
+
+    function getGeometriesExtents(geometries) {
+      return geometries.reduce(({min, max}, {data}) => {
+        const minMax = getExtents(data.position);
+        return {
+          min: min.map((min, ndx) => Math.min(minMax.min[ndx], min)),
+          max: max.map((max, ndx) => Math.max(minMax.max[ndx], max)),
+        };
+      }, {
+        min: Array(3).fill(Number.POSITIVE_INFINITY),
+        max: Array(3).fill(Number.NEGATIVE_INFINITY),
+      });
+    }
+
+    const extents = getGeometriesExtents(obj.geometries);
+    const range = m4.subtractVectors(extents.max, extents.min);
+    globalRange = range;
+    // amount to move the object so its center is at the origin
+    const objOffset = m4.scaleVector(
+        m4.addVectors(
+          extents.min,
+          m4.scaleVector(range, 0.5)),
+        -1);
+
+    //Preparation to draw it//
+
+    var texcoordAttributeLocation = gl.getAttribLocation(meshProgramInfo.program, "a_texcoord");
+    gl.enableVertexAttribArray(texcoordAttributeLocation);
+
+    // Tell the attribute how to get data out of texcoordBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floating point values
+    var normalize = true;  // convert from 0-255 to 0.0-1.0
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next color
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        texcoordAttributeLocation, size, type, normalize, stride, offset);
+
+    // Create a texture.
+    var texture = gl.createTexture();
+
+    // use texture unit 0
+    gl.activeTexture(gl.TEXTURE0 + 0);
+
+    // bind to the TEXTURE_2D bind point of texture unit 0
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Fill the texture with a 1x1 blue pixel.
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+                  new Uint8Array([0, 0, 255, 255]));
+
+    // Asynchronously load an image
+    var image = new Image();
+    image.src = itensCarrinho[i].texture;
+    image.addEventListener('load', function() {
+      // Now that the image has loaded make copy it to the texture.
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+      gl.generateMipmap(gl.TEXTURE_2D);
+    });
+  }
+  
+  function radToDeg(r) {
+    return r * 180 / Math.PI;
+  }
+  
+  function degToRad(deg) {
+    return deg * Math.PI / 180;
+  }
+
+  //Set da camera//
+  const cameraTarget = [0, 0, 0];
+  // figure out how far away to move the camera so we can likely
+  // see the object.
+  const radius = m4.length(globalRange) * 1.2;
+  const cameraPosition = m4.addVectors(cameraTarget, [
+    0,
+    0,
+    radius,
+  ]);
+  // Set zNear and zFar to something hopefully appropriate
+  // for the size of this object.
+  const zNear = radius / 100;
+  const zFar = radius * 3;
+
+    var translation = [0, 0, 0];
+    var rotation = [degToRad(0), degToRad(0), degToRad(0)];
+    var scale = [1, 1, 1];
+
+  //Fim do set da camera//
+
+  requestAnimationFrame(render);
+
+  function render(time) {
+    let animate = true;
+    let speed;
+    if (animate){
+      speed = 0.001;
+    } else {
+      speed = 0;
+    }
+    time *= speed;  // convert to seconds
+
+    twgl.resizeCanvasToDisplaySize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(1, 1, 1, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
+    const fieldOfViewRadians = degToRad(60);
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const projection = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
+
+    const up = [0, 1, 0];
+    // Compute the camera's matrix using look at.
+    const camera = m4.lookAt(cameraPosition, cameraTarget, up);
+
+    // Make a view matrix from the camera matrix.
+    const view = m4.inverse(camera);
+
+    const sharedUniforms = {
+      u_lightDirection: m4.normalize([-1, 3, 5]),
+      u_view: view,
+      u_projection: projection,
+    };
+
+    gl.useProgram(meshProgramInfo.program);
+
+    // calls gl.uniform
+    twgl.setUniforms(meshProgramInfo, sharedUniforms);
+
+    // compute the world matrix once since all parts
+    // are at the same space.
+    
+    let u_world = m4.yRotation(time);
+    m4.xRotate(u_world, degToRad(0), u_world);                          //Sets initial orientation
+    m4.scale(u_world, scale[0], scale[1], scale[2], u_world);//sets initial scale
+    m4.translate(u_world, translation[0], translation[2], translation[1], u_world);
+    m4.xRotate(u_world, rotation[0], u_world);
+    m4.yRotate(u_world, rotation[1], u_world);
+    m4.zRotate(u_world, rotation[2], u_world);
+      
+    for (let i = 0; i<carrinhoQTD; i++){
+      for (const {bufferInfo, vao, material} of globalParts[i]) {
+        // set the attributes for this part.
+        gl.bindVertexArray(vao);
+        // calls gl.uniform
+        twgl.setUniforms(meshProgramInfo, {
+          u_world,
+          u_diffuse: material.u_diffuse,
+        });
+        // calls gl.drawArrays or gl.drawElements
+        twgl.drawBufferInfo(gl, bufferInfo);
+      }
+      m4.translate(u_world, 1, 0, 0, u_world);
+    }
+    requestAnimationFrame(render);
+  }
 }
 
 main();
